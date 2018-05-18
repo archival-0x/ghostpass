f3a9794adc64da7ad4f6acc5ed7730ca6b7e6804
ex0dus-0x <ex0dus@codemuch.tech>
2018-05-17 21:10:01 -0400

    model enhacements

---
+++ src/crypto.py
@@ -0,0 +1,12 @@
+import hashlib
+from Crypto.Cipher import AES
+
+class CryptoHelper:
+    def __init__(password):
+        self.password = password
+
+    def encrypt(self):
+        return self
+
+    def decrypt(self):
+        return self
