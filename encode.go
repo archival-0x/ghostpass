c65d3c1dbae00104dc339173435974ec62a431b4
ex0dus-0x <ex0dus@codemuch.tech>
2020-06-20 23:12:23 -0400

    Tune up textual steganography

--- encode.go
+++ encode.go
@@ -1,6 +1,9 @@
 package ghostpass
 
-import "strings"
+import (
+    "strings"
+    "strconv"
+)
 
 const (
     // ZWJ represents zero-width joiner.
@@ -10,9 +13,44 @@ const (
     ZWNJ = '\u200C'
 )
 
+// TODO: construct error
 
-// Checks to see if a given corpus file contains zero-width characters
+// Checks to see if a given corpus file contains zero-width characters already
 func ContainsHiddenChars(corpus string) bool {
 	return strings.ContainsRune(corpus, ZWJ) ||
 		strings.ContainsRune(corpus, ZWNJ)
+}
+
+// given a plaintext string corpus and a secret to hide, encode it with zero-width characters
+func EncodeHiddenString(plain string, secret string) string {
+    // convert secret string into binary representation
+    var binary []byte
+    for _, c := range secret {
+        binary = strconv.AppendInt(binary, int64(c), 2)
+    }
+
+    // use a strings builder to push unicode characters from binary bytearray
+    var corpus strings.Builder
+    corpus.WriteString(plain)
+    for b := range binary {
+        if b == 1 {
+            corpus.WriteString(string(ZWJ))
+        } else if b == 0 {
+            corpus.WriteString(string(ZWNJ))
+        }
+    }
+
+    // return finalized string representation
+    return corpus.String()
+}
+
+func DecodeHiddenString(corpus string) []byte {
+    var result []byte
+
+    // iterate through corpus and parse out zero-width unicode chars
+    for b := range byte[](corpus) {
+
+    }
+
+    return result
 }
